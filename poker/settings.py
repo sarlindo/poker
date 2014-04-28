64d98d34c4eecd00b0411cda4baafc254729131b
root <root@plumdj01.(none)>
2014-04-28 13:21:23 -0400

    Initial commit

--- poker/settings.py
+++ poker/settings.py
@@ -93,5 +93,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+# Static asset configuration
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
