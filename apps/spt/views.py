79e400d6746072827a364e012105a61971d6da8e
root <root@plumdj01.(none)>
2014-04-29 10:51:33 -0400

    Initial commit

--- apps/spt/views.py
+++ apps/spt/views.py
@@ -117,6 +117,7 @@ def index(request):
                         gm.seasons_id=se.seasonnumber and
                         gm.seasons_id=(select MAX(seasonnumber) from spt_season) and
                         cast(gm.finalseasongame as int) = 0
+			group by gm.seasons_id
                         """))
 	newsflash_list = list(query_to_dicts("""
                         select newsflash
