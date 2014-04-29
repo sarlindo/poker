b84a5253f52b2ecd4e270adfde4b294a719bc66f
root <root@plumdj01.(none)>
2014-04-29 11:41:47 -0400

    Initial commit

--- poker/urls.py
+++ poker/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'spt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^spt/', include('apps.spt.urls')),
+    url(r'^$', include('apps.spt.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
