efd5fda35e64e94c03a4fc076c303080fd49c414
root <root@plumdj01.(none)>
2015-02-15 16:58:30 -0500

    Initial commit

--- apps/spt/views.py
+++ apps/spt/views.py
@@ -62,7 +62,9 @@ def index(request):
                         pl.firstname as plleader,
                         se.seasonnumber,
                         sum(gp.point) as points,
-                        (count(*) * 10) as buyintotal
+                        (count(*) * 10) as buyintotal,
+			SUM(CASE WHEN cast(gp.placement as int) = 1 THEN 1 ELSE 0 END) as FirstPlaces,
+                        SUM(CASE WHEN cast(gp.placement as int) = 2 THEN 1 ELSE 0 END) as SecondPlaces
                         from spt_player pl, spt_play gp, spt_game gm, spt_season se
                         where
 			pl.firstname = gp.players_id and
@@ -72,7 +74,7 @@ def index(request):
                         cast(gm.finalseasongame as int) = 0 and
                         cast(gp.sptmember as int) = 1
 			group by pl.firstname,se.seasonnumber
-                        order by points desc limit 1) plpa               
+                        order by points desc, FirstPlaces desc, SecondPlaces desc limit 1) plpa               
 			where
                         pl.firstname = gp.players_id and
                         gp.games_id=gm.id and
