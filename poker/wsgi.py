433c3db685becb00df56548286fff0e532d924e0
root <root@plumdj01.(none)>
2017-07-01 23:18:31 -0400

    spt website refresh

--- poker/wsgi.py
+++ poker/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poker.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
